1937ade92720e2af85310192e6b7964540fafa20
Vaiva <vaivaug@gmail.com>
2020-01-14 15:30:10 +0000

    add prepare_data file to read noteevents and admission tables, merge, remove newborn, add output column

--- Main.py
+++ Main.py
@@ -2,44 +2,13 @@
 import pandas as pd
 import csv
 from read_data import *
-
-# from textblob import TextBlob
-# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-filedir_notes = '../NOTEEVENTS.csv'
-filedir_notes_subset = '../NOTEEVENTS_SUBSET.csv'
-filedir_admissions = '../ADMISSIONS.csv'
-
-df_notes = read_file(filedir_notes)
-df_admissions = read_file(filedir_admissions)
-
-diagnosis_contains_cancer = df_admissions[df_admissions['DIAGNOSIS'].str.contains('LUNG CA', na=False)]
-print(diagnosis_contains_cancer.count())
-
-df_notes_dis_sum = df_notes.loc[df_notes.CATEGORY == 'Discharge summary']
-print('Discharge summary', len(df_notes_dis_sum))
-
-result = get_df_merged(df_notes, df_admissions)
-result.head(100).to_csv('../MERGED.csv')
-
-print(result)
-print(len(result))
+from prepare_data import get_clean_dataframe
 
 
-'''
-print(len(df_notes.groupby(['SUBJECT_ID'], as_index=False)['TEXT'].sum()))
-print(len(df_notes.groupby(['HADM_ID'], as_index=False)['TEXT'].sum()))
-print('admissions')
-print(len(df_admissions))
-print(len(df_admissions.groupby(['HADM_ID'], as_index=False).sum()))
-'''
+notes_adm = get_clean_dataframe()
+print(notes_adm)
+print(len(notes_adm))
 
 
-'''
-patients_counter = 0
-for i in data['SUBJECT_ID']:
-    patients_counter += 1
-    print(i)
 
-print("patients counter out of 1000 rows: ", patients_counter)
-'''
+
---
+++ prepare_data.py
@@ -0,0 +1,84 @@
+'''
+Contains functions to read the data, select 'dischange summaries', merge tables, add Output column
+'''
+
+import pandas as pd
+import nltk
+nltk.download('punkt')
+
+
+filedir_notes = '../NOTEEVENTS.csv'
+filedir_adm = '../ADMISSIONS.csv'
+
+
+def get_clean_dataframe():
+
+    adm = get_adm_dataframe()
+    notes = get_notes_dataframe()
+
+    # check there is only one discharge summary per person. Can delete this row later
+    assert notes.duplicated(['HADM_ID']).sum() == 0, 'Multiple discharge summaries per admission'
+
+    notes_adm = get_merged_dataframe(notes, adm)
+    notes_adm = get_dataframe_with_outputs(notes_adm)
+    notes_adm = get_dataframe_no_newborn(notes_adm, adm)
+    return notes_adm
+
+
+def get_adm_dataframe():
+    # read admissions table
+    adm = pd.read_csv(filedir_adm)
+    adm = adm.sort_values(['SUBJECT_ID'])
+    adm = adm.reset_index(drop=True)
+    return adm
+
+
+def get_notes_dataframe():
+    # read noteevenets table
+    notes = pd.read_csv(filedir_notes, low_memory=False)
+    # select only the discharge summary column
+    notes_dis_sum = notes.loc[notes.CATEGORY == 'Discharge summary']
+    # select the last 'discharge summary'.
+    '''TODO: try joining the discharge summaries where multiple exist'''
+    notes_dis_sum = (notes_dis_sum.groupby(['SUBJECT_ID', 'HADM_ID']).nth(-1)).reset_index()
+    return notes_dis_sum
+
+'''
+# Select duplicate rows except first occurrence based on all columns
+duplicated = notes_dis_sum[notes_dis_sum.duplicated(subset=['HADM_ID'], keep=False)]
+duplicated.to_csv('DUPLICATED.csv')
+
+print("Duplicate Rows except first occurrence based on all columns are :")
+print(duplicated)
+'''
+
+
+def get_merged_dataframe(notes, adm):
+
+    notes_adm = pd.merge(adm[['HADM_ID','DIAGNOSIS']],
+                            notes[['HADM_ID','TEXT']],
+                            on=['HADM_ID'],
+                            how='left')
+    return notes_adm
+
+
+def get_dataframe_with_outputs(notes_adm):
+    '''TODO: can be improved by adding more keywords meaning lung cancer.'''
+    notes_adm['OUTPUT'] = (notes_adm.DIAGNOSIS.str.contains('LUNG CA') |
+                                (notes_adm.DIAGNOSIS.str.contains('LUNG', na=False) &
+                                notes_adm.DIAGNOSIS.str.contains('TUMOR', na=False)) |
+                                (notes_adm.DIAGNOSIS.str.contains('LUNG', na=False) &
+                                notes_adm.DIAGNOSIS.str.contains('CANCER', na=False))
+                                ).astype('int')
+    return notes_adm
+
+
+def get_dataframe_no_newborn(notes_adm, adm):
+    # remove newborn
+    notes_adm_final = notes_adm[notes_adm.DIAGNOSIS.str.contains('NEWBORN')==False]
+    print(notes_adm_final)
+    print(notes_adm_final.groupby('OUTPUT').count())
+    # check number of rows is the same. Can delete later
+    assert len(adm) == len(notes_adm), 'Number of rows is higher'
+    return notes_adm_final
+
