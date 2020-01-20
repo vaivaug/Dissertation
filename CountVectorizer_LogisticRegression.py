85b3999738dff5fe6f9c08f36f1609854f00283e
Vaiva <vaivaug@gmail.com>
2020-01-20 19:05:10 +0000

    add extraction of the most important words with weights

--- CountVectorizer_LogisticRegression.py
+++ CountVectorizer_LogisticRegression.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import roc_auc_score
 import numpy as np
+import collections
 
-global train_TEXT, test_TEXT, train_OUTPUT, test_OUTPUT, model, predicted_OUTPUT
+global train_TEXT, test_TEXT, train_OUTPUT, test_OUTPUT, model, predicted_OUTPUT, list_words
 
 
 def get_test_predicted_OUTPUT(train, test):
 
-    global train_TEXT, test_TEXT, train_OUTPUT, test_OUTPUT, model, predicted_OUTPUT
+    global train_TEXT, test_TEXT, train_OUTPUT, test_OUTPUT, model, predicted_OUTPUT, list_words
 
     # remove new line characters and nulls
     train = get_clean_text(train)
@@ -23,7 +24,7 @@ def get_test_predicted_OUTPUT(train, test):
     print("this can take longer")
     vectorizer.fit_transform(train.TEXT.values)
 
-    train_TEXT = vectorizer.transform(train.TEXT.values)
+    train_TEXT = vectorizer.fit_transform(train.TEXT.values)
     test_TEXT = vectorizer.transform(test.TEXT.values)
     print(type(test_TEXT))
 
@@ -33,6 +34,8 @@ def get_test_predicted_OUTPUT(train, test):
     # logistic regression
     model = LogisticRegression(C=0.0001, penalty='l2')
     model.fit(train_TEXT, train_OUTPUT)
+
+    list_words = vectorizer.get_feature_names()
 
     # train_OUTPUT_preds = model.predict_proba(train_TEXT)[:, 1]
     # test_OUTPUT_preds = model.predict_proba(test_TEXT)[:, 1]
@@ -82,4 +85,34 @@ def plot_AUC(test_OUTPUT):
     plt.legend()
     # show the plot
     plt.show()
+
+
+def plot_word_importance():
+
+    # weights associated to words in list_words
+    weights = model.coef_
+    abs_weights = np.abs(weights)
+    print('WEIGHTS:')
+    print(abs_weights)
+
+    # join words with weight values
+    joined_word_weight = dict(zip(list_words, weights[0]))
+
+    # sort words by weight from the lowest to the highest i.e. from negative to positive importance
+    sorted_word_weight = {k: v for k, v in sorted(joined_word_weight.items(),
+                                                 key=lambda item: item[1])}
+    print(sorted_word_weight)
+
+    positive_importance = {}
+    negative_importance = {}
+
+    for word in list(sorted_word_weight)[0:20]:
+        negative_importance[word] = sorted_word_weight[word]
+
+    print(negative_importance)
+
+    for word in list(reversed(list(sorted_word_weight)))[0:20]:
+        positive_importance[word] = sorted_word_weight[word]
+
+    print(positive_importance)
 
--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from train_test_data import get_train_test_datasets
 from sub_sampling_negatives import get_sub_sampling_negatives_data
 import nltk
 nltk.download('punkt')
-from CountVectorizer_LogisticRegression import get_test_predicted_OUTPUT, plot_AUC
+from CountVectorizer_LogisticRegression import get_test_predicted_OUTPUT, plot_AUC, plot_word_importance
 from confusion_matrix import *
 from sick_ones_to_file import write_sick_ones_to_file
 
@@ -12,6 +12,7 @@ from sick_ones_to_file import write_sick_ones_to_file
 1. joined discharge summaries    DONE
 2. check which words have the biggest influence for 1 or 0 output prediction
 3. why empty discharge summary predicts lung cancer. Possibly some of the lung cancer diagnoses have am empty discharge sumary
+4. add ngram_range=(1,3) inside CountVectorizer 
 '''
 notes_adm = get_clean_dataframe()
 
@@ -25,6 +26,7 @@ train = get_sub_sampling_negatives_data(train)
 
 test_OUTPUT, predicted_OUTPUT = get_test_predicted_OUTPUT(train, test)
 
+plot_word_importance()
 # confusion matrix
 cnf_matrix = get_confusion_matrix(test_OUTPUT, predicted_OUTPUT)
 plot_confusion_matrix(cnf_matrix)
@@ -34,6 +36,7 @@ print("Precision:", metrics.precision_score(test_OUTPUT, predicted_OUTPUT))
 print("Recall:", metrics.recall_score(test_OUTPUT, predicted_OUTPUT))
 
 plot_AUC(test_OUTPUT)
+
 
 
 
