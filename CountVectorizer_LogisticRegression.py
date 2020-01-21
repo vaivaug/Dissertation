6d52c6d7137139d56c5b09bbeca4486fb8b22410
Vaiva <vaivaug@gmail.com>
2020-01-21 00:10:17 +0000

    code with n_grams in CountVectorizer, frac=0.2, no empty TEXT fields

--- CountVectorizer_LogisticRegression.py
+++ CountVectorizer_LogisticRegression.py
@@ -10,23 +10,27 @@ import collections
 global train_TEXT, test_TEXT, train_OUTPUT, test_OUTPUT, model, predicted_OUTPUT, list_words
 
 
-def get_test_predicted_OUTPUT(train, test):
+def get_test_predicted_OUTPUT(train, test, threshold):
 
     global train_TEXT, test_TEXT, train_OUTPUT, test_OUTPUT, model, predicted_OUTPUT, list_words
 
     # remove new line characters and nulls
     train = get_clean_text(train)
     test = get_clean_text(test)
+    print('lengths:')
+    print(len(train))
+    print(len(test))
 
     # vectorizer creation
-    vectorizer = CountVectorizer(max_features=3000, tokenizer=get_tokenizer, stop_words=get_stop_words())
+    vectorizer = CountVectorizer(max_features=3000, tokenizer=get_tokenizer, stop_words=get_stop_words(), ngram_range=(1, 3))
 
     print("this can take longer")
+    # learn the vocabulary dictionary
     vectorizer.fit_transform(train.TEXT.values)
 
+    # return term-document matrix
     train_TEXT = vectorizer.fit_transform(train.TEXT.values)
     test_TEXT = vectorizer.transform(test.TEXT.values)
-    print(type(test_TEXT))
 
     train_OUTPUT = train.OUTPUT
     test_OUTPUT = test.OUTPUT
@@ -37,11 +41,8 @@ def get_test_predicted_OUTPUT(train, test):
 
     list_words = vectorizer.get_feature_names()
 
-    # train_OUTPUT_preds = model.predict_proba(train_TEXT)[:, 1]
-    # test_OUTPUT_preds = model.predict_proba(test_TEXT)[:, 1]
-
-    predicted_OUTPUT = model.predict(test_TEXT)
-
+    predicted_OUTPUT = np.where(model.predict_proba(test_TEXT)[:, 1] > threshold, 1, 0)
+    '''
     print('******************text*****************')
     test.TEXT.to_csv('../TEST_TEXT.csv')
 
@@ -51,6 +52,8 @@ def get_test_predicted_OUTPUT(train, test):
     print(test.OUTPUT.iloc[[2]])
     print('******************PREDICTED OUTOUT*********************')
     np.savetxt("../test_predicted_output.csv", predicted_OUTPUT, delimiter=",")
+   
+    '''
     return test_OUTPUT, predicted_OUTPUT
 
 
@@ -89,6 +92,27 @@ def plot_AUC(test_OUTPUT):
 
 def plot_word_importance():
 
+    sorted_word_weight = get_sorted_word_importance_dict()
+
+    positive_importance = {}
+    negative_importance = {}
+
+    for word in list(sorted_word_weight)[0:30]:
+        negative_importance[word] = sorted_word_weight[word]
+
+    print(negative_importance)
+
+    for word in list(reversed(list(sorted_word_weight)))[0:30]:
+        positive_importance[word] = sorted_word_weight[word]
+
+    print(positive_importance)
+
+    plot_one_side_importance(positive_importance)
+    plot_one_side_importance(negative_importance)
+
+
+def get_sorted_word_importance_dict():
+
     # weights associated to words in list_words
     weights = model.coef_
     abs_weights = np.abs(weights)
@@ -100,19 +124,19 @@ def plot_word_importance():
 
     # sort words by weight from the lowest to the highest i.e. from negative to positive importance
     sorted_word_weight = {k: v for k, v in sorted(joined_word_weight.items(),
-                                                 key=lambda item: item[1])}
-    print(sorted_word_weight)
+                                                  key=lambda item: item[1])}
+    return sorted_word_weight
 
-    positive_importance = {}
-    negative_importance = {}
 
-    for word in list(sorted_word_weight)[0:20]:
-        negative_importance[word] = sorted_word_weight[word]
+def plot_one_side_importance(importance_dict):
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
 
-    print(negative_importance)
-
-    for word in list(reversed(list(sorted_word_weight)))[0:20]:
-        positive_importance[word] = sorted_word_weight[word]
-
-    print(positive_importance)
-
+    ax.barh(range(len(importance_dict)), list(importance_dict.values()), align='center')
+    ax.set_yticks(range(len(importance_dict)))
+    ax.set_yticklabels(list(importance_dict.keys()))
+    ax.invert_yaxis()
+    ax.set_xlabel('words')
+    ax.set_title('Importance of words')
+    plt.show()
+    ax.invert_yaxis()
--- Main.py
+++ Main.py
@@ -10,21 +10,27 @@ from sick_ones_to_file import write_sick_ones_to_file
 
 '''
 1. joined discharge summaries    DONE
-2. check which words have the biggest influence for 1 or 0 output prediction
-3. why empty discharge summary predicts lung cancer. Possibly some of the lung cancer diagnoses have am empty discharge sumary
-4. add ngram_range=(1,3) inside CountVectorizer 
+2. check which words have the biggest influence for 1 or 0 output prediction DONE
+3. why empty discharge summary predicts lung cancer. Possibly some of the lung cancer diagnoses have am empty discharge sumary DONE 
+4. add ngram_range=(1,3) inside CountVectorizer
+5. look into which files are at the top right
+6. remove empty ones from all data
 '''
 notes_adm = get_clean_dataframe()
+notes_adm.TEXT = notes_adm.TEXT.str.replace('\n','')
+notes_adm['TEXT'].replace('', np.nan, inplace=True)
+notes_adm.dropna(subset=['TEXT'], inplace=True)
 
-# write_sick_ones_to_file('../sick_ones.csv', notes_adm)
+write_sick_ones_to_file('../sick_ones.csv', notes_adm)
 
 print(len(notes_adm))
 
+# all data split into train and test
 train, test = get_train_test_datasets(notes_adm)
 
 train = get_sub_sampling_negatives_data(train)
 
-test_OUTPUT, predicted_OUTPUT = get_test_predicted_OUTPUT(train, test)
+test_OUTPUT, predicted_OUTPUT = get_test_predicted_OUTPUT(train, test, threshold=0.4)
 
 plot_word_importance()
 # confusion matrix
