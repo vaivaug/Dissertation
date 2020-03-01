c6c20a17395e52ab42c55f4a6f7a2d3f3086aac1
va29 <vaivaug@gmail.com>
2020-03-01 12:46:38 +0000

    add GUI select model, data balancing type and enter threshold. Save values when run model button is pressed

--- Gui.py
+++ Gui.py
@@ -7,46 +7,49 @@ window.title("Set up program parameters")
 window.geometry('600x400')
 
 
-# Threshold
-threshold_label = Label(window, text="  Threshold: ")
-threshold_label.grid(column=0, row=0)
-txt = Entry(window,width=12)
-txt.grid(column=1, row=0)
+# threshold label
+threshold_label = Label(window, text="  Threshold:")
+threshold_label.config(font=("Courier bold", 12))
+threshold_label.grid(column=0, row=0, sticky=W, padx=10, pady=10)
+
+# enter threshold window
+txt = Entry(window, width=12)
+txt.grid(column=1, row=0, sticky=W, padx=10, pady=10)
+
+
+# label
+data_balancing_label = Label(window, text="  Train data balancing type: ")
+data_balancing_label.config(font=("Courier bold", 12))
+data_balancing_label.grid(column=0, row=2, sticky=W, padx=10, pady=10)
+data_balancing_type = Combobox(window)
+data_balancing_type['values']= ("SMOTE", "sub-sample negatives", "over-sample positives")
+data_balancing_type.current(0)
+data_balancing_type.grid(column=1, row=2, padx=10, pady=10)
+
+
+# label
+model_label = Label(window, text="  Model: ")
+model_label.config(font=("Courier bold", 12))
+model_label.grid(column=0, row=4, sticky=W, padx=10, pady=10)
+
+
+model_type = Combobox(window)
+model_type['values']= ("Logistic Regression", "SVM")
+model_type.current(0)
+
+model_type.grid(column=1, row=4, padx=10, pady=10)
+
 
 def clicked():
 
     threshold = txt.get()
-    # lbl.configure(text= res)
-
-# btn = Button(window, text="Click Me", command=clicked)
-
-# btn.grid(column=2, row=0)
+    balancing_type = data_balancing_type.get()
+    model = model_type.get()
+    print(model, '   ', balancing_type, '  ', threshold)
 
 
-var1 = StringVar()
-var2 = StringVar()
+run_button = Button(window, text="Run Model", command=clicked)
+run_button.grid(column=1, row=5, padx=10, pady=10)
 
-var1.set(0)
-var2.set(0)
-
-
-data_balancing_label = Label(window, text="  Train data balancing type: ")
-data_balancing_label.grid(column=0, row=1)
-
-data_balance_type1 = Radiobutton(window,text='SMOTE', variable=var1, value=1)
-data_balance_type2 = Radiobutton(window,text='sub-sample negatives', variable=var1, value=2)
-data_balance_type3 = Radiobutton(window,text='over-sample positives', variable=var1, value=3)
-
-data_balance_type1.grid(column=0, row=2)
-data_balance_type2.grid(column=1, row=2)
-data_balance_type3.grid(column=2, row=2)
-
-rad1 = Radiobutton(window,text='1', variable=var2, value=4)
-rad2 = Radiobutton(window,text='2', variable=var2, value=5)
-rad3 = Radiobutton(window,text='3', variable=var2, value=6)
-
-rad1.grid(column=0, row=3)
-rad2.grid(column=1, row=3)
-rad3.grid(column=2, row=3)
 
 window.mainloop()
