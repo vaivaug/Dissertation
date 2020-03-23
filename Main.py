27546c50c5cc91b4c0a154abc9e0270be732864f
vaivaug <vaivaug@gmail.com>
2020-03-23 01:01:46 +0000

    add completed experiments plot, compare training cross validation run and validation set predictions of a few runs to make sure we are not overfitting

--- Main.py
+++ Main.py
@@ -2,24 +2,34 @@
 Main file to run the code. Two options: enter parameters using Gui or set parameters manually
 """
 from Gui import run_gui
-from program_run_start_to_end import predict_test_validation_set, plot_evaluation_metrics
+from program_run_start_to_end import predict_test_validation_set, plot_evaluation_metrics, predict_cross_val_train_set
 
+from experiments import plot
+from evaluation.confusion_matrix import get_confusion_matrix
 
 # variable to know if parameters are entered from Gui or manually
 parameters_set_manually = True
 
 if parameters_set_manually:
 
-    threshold = 0.5
+    threshold = 0.4
     balancing_types = ['SMOTE', 'sub-sample negatives', 'over-sample positives']
     solvers = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']
     ngram_min = 1
     ngram_max = 1
+    print('on validation set: ')
     test_OUTPUT, predicted_OUTPUT, prediction_probs = predict_test_validation_set(threshold, balancing_types[1],
-                                                                             solvers[1], ngram_min, ngram_max)
+                                                                             solvers[2], ngram_min, ngram_max)
     # plot confusion_matrix, AUC, print accuracy
-    plot_evaluation_metrics(test_OUTPUT, predicted_OUTPUT, prediction_probs)
+    plot(test_OUTPUT, predicted_OUTPUT, prediction_probs, balancing_types[1], solvers[1], threshold, ngram_min, ngram_max)
+    get_confusion_matrix(test_OUTPUT, predicted_OUTPUT)
 
+    print('on train cross validation: ')
+    test_OUTPUT, predicted_OUTPUT, prediction_probs = predict_cross_val_train_set(threshold, balancing_types[1],
+                                                                                  solvers[2], ngram_min, ngram_max)
+    # plot confusion_matrix, AUC, print accuracy
+    plot(test_OUTPUT, predicted_OUTPUT, prediction_probs, balancing_types[1], solvers[1], threshold, ngram_min, ngram_max)
+    get_confusion_matrix(test_OUTPUT, predicted_OUTPUT)
 else:
     # enter parameters from Gui
     run_gui()
--- experiments.py
+++ experiments.py
@@ -1,4 +1,4 @@
-from program_run_start_to_end import predict_cross_val_train_set
+from program_run_start_to_end import predict_cross_val_train_set, predict_test_validation_set
 import numpy as np
 import sklearn.metrics as metrics
 import matplotlib.pyplot as plt
@@ -16,12 +16,12 @@ def run_experiment_balance_solver(balancing_type, solver):
     for threshold in thresholds_list:
         for ngram in ngrams_list:
 
-            train_OUTPUT, predicted_OUTPUT, prediction_probs = predict_cross_val_train_set(0.4,
+            train_OUTPUT, predicted_OUTPUT, prediction_probs = predict_test_validation_set(0.4,
                                                                                           balancing_type,
                                                                                           solver,
                                                                                           ngram[0],
                                                                                           ngram[1])
-            plot(train_OUTPUT, predicted_OUTPUT, prediction_probs, balancing_type, solver, threshold, ngram[0],
+            plot(train_OUTPUT, predicted_OUTPUT, prediction_probs, balancing_type, solver, 0.4, ngram[0],
                                   ngram[1])
             break
         break
@@ -42,90 +42,63 @@ def run_all_experiments():
 def plot(test_OUTPUT, predicted_OUTPUT, prediction_probs, balancing_type, solver, threshold, ngram_min, ngram_max):
 
     fig, ax = plt.subplots()
+
     # true positive rate
     recall = metrics.recall_score(test_OUTPUT, predicted_OUTPUT)
 
     # no skill prediction
     no_skill_probs = [0 for _ in range(len(test_OUTPUT))]
 
-    # calculate scores
-    no_skill_auc = metrics.roc_auc_score(test_OUTPUT, no_skill_probs)
+    # calculate AUC score
     model_auc = metrics.roc_auc_score(test_OUTPUT, prediction_probs)
 
     # calculate roc curves
-    no_skills_false_pos_rate, no_skill_true_pos_rate, no_skill_thresholds = metrics.roc_curve(test_OUTPUT, no_skill_probs)
-    model_false_positive_rate, model_true_pos_rate, model_thresholds = metrics.roc_curve(test_OUTPUT, prediction_probs)
-
+    no_skills_false_pos_rate, no_skill_true_pos_rate, no_skill_thresholds = metrics.roc_curve(test_OUTPUT,
+                                                                                              no_skill_probs)
+    model_false_positive_rate, model_true_pos_rate, model_thresholds = metrics.roc_curve(test_OUTPUT,
+                                                                                         prediction_probs)
+    # string to be outputted in a text box
     experiment_params = '\n'.join((
         'Balancing type:  {}'.format(balancing_type),
         'Solver:  {}'.format(solver),
         'Threshold:  {}'.format(threshold),
         'N-grams:  ({}, {})'.format(ngram_min, ngram_max),
         'AUC:  %.3f' % (model_auc,),
-        'Recall:  %.3f' % (recall,),
+        'Recall:  %.3f' % (recall,)
     ))
 
+    # plot the auc curves
     ax.plot(model_false_positive_rate, model_true_pos_rate, marker='.', label='Logistic')
     ax.plot(no_skills_false_pos_rate, no_skill_true_pos_rate, linestyle='--', label='No skills')
-    # these are matplotlib.patch.Patch properties
-    props = dict(boxstyle='round', facecolor='none', edgecolor='black', alpha=0.7)
+    props = dict(boxstyle='round', facecolor='none', edgecolor='grey', alpha=0.7)
 
-    # place a text box in upper left in axes coords
-    ax.text(0.41, 0.3, experiment_params, transform=ax.transAxes, fontsize=10,
+    # place a text box in the bottom right
+    x, y = get_text_coordinates(balancing_type)
+    ax.text(x, y, experiment_params, transform=ax.transAxes, fontsize=10,
             verticalalignment='top', bbox=props)
 
     # axis labels
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.legend(loc='best')
-
-    plt.show()
-
-
-def plot_evaluation_image(test_OUTPUT, predicted_OUTPUT, prediction_probs, balancing_type, solver, ngram_min, ngram_max):
-
-    # true positive rate
-    recall = metrics.recall_score(test_OUTPUT, predicted_OUTPUT)
-
-    model_auc = metrics.roc_auc_score(test_OUTPUT, prediction_probs)
-    print('Logistic: ROC AUC=%.3f' % model_auc)
-
-    # calculate roc curves
-    model_false_positive_rate, model_true_pos_rate, model_thresholds = metrics.roc_curve(test_OUTPUT, prediction_probs)
-
-    # plot the roc curve for the model
-    plt.plot(model_false_positive_rate, model_true_pos_rate, marker='.', label='Logistic Regression')
     plt.title('Area Under the ROC Curve')
+    plt.legend(bbox_to_anchor=(0.97, 0.35), loc='lower right', edgecolor='grey')
 
-    font = {'family': 'serif',
-            'color': 'darkred',
-            'weight': 'normal',
-            'size': 16,
-            }
+    plt_fig = plt.gcf()
+    plt_fig.tight_layout()
+    plt_fig.savefig('experiment_plots/{}-{}-{}-({},{}).png'.format(balancing_type, solver, threshold, ngram_min, ngram_max))
 
-   # plt.text(6, 6, 'some text\n another line', fontdict=font)
-
-    # axis labels
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-
-    # show the legend
-    plt.legend(title='Recall: {}\nAUC={}' .format(recall, round(model_auc, 3)), loc='lower right')
-    # Tweak spacing to prevent clipping of ylabel
-   #  plt.subplots_adjust(left=0.15)
     plt.show()
 
-    '''
-    plt.tight_layout()
-    # show the plot
-    auc_fig = plt.gcf()
-    auc_fig.tight_layout()
-    plt.show()
-    plt.draw()
-    auc_fig.savefig('plots/auc_plt.png')
-    plt.clf()
-    '''
+
+def get_text_coordinates(balancing_type):
+
+    if balancing_type == 'SMOTE':
+        return 0.65, 0.3
+    elif balancing_type == 'over-sample positives':
+        return 0.465, 0.3
+    else:
+        return 0.465, 0.3
 
 
 # run_all_experiments()
-run_experiment_balance_solver('sub-sample negatives', 'lbfgs')
+# run_experiment_balance_solver('sub-sample negatives', 'lbfgs')
